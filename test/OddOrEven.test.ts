2a0c3360cbc31db3b3a6161b0958d9d6037dfa14
carlosamcruz <carlosamcruz@ufam.edu.br>
2024-11-19 17:40:31 -0300

    Iniciando testes

--- test/OddOrEven.test.ts
+++ test/OddOrEven.test.ts
@@ -6,6 +6,35 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre from "hardhat";
 
+type GameData = {
+  hashOptionP1: string; // Hash of Player 1's option
+  timeOut: string;      // Timeout duration in seconds (uint64)
+  timeOutP1: string;    // Player 1 timeout timestamp (uint256)
+  timeOutP2: string;    // Player 2 timeout timestamp (uint256)
+  nLockTime: string;    // Lock time for the game (uint256)
+  isOdd: boolean;       // Whether the game is Odd/Even (bool)
+  player1: string;      // Player 1's address (address)
+  player2: string;      // Player 2's address (address)
+  optionP2: number;     // Player 2's option (int8)
+};
+
+function fetchGameData(rawGameData: any) {
+  
+  const gameData: GameData = {
+    hashOptionP1: rawGameData[0],
+    timeOut: rawGameData[1],
+    timeOutP1: rawGameData[2],
+    timeOutP2: rawGameData[3],
+    nLockTime: rawGameData[4],
+    isOdd: rawGameData[5],
+    player1: rawGameData[6],
+    player2: rawGameData[7],
+    optionP2: Number(rawGameData[8]),
+  };
+  return gameData;
+}
+
+
 function hexStringToUint8Array(hexString: string): Uint8Array {
   // Ensure the hex string length is even
   if (hexString.length % 2 !== 0) {
@@ -26,35 +55,8 @@ function hexStringToUint8Array(hexString: string): Uint8Array {
 
 describe("OddOrEven", function () {
 
-
-  type GameData = {
-    hashOptionP1: string; // Hash of Player 1's option
-    timeOut: string;      // Timeout duration in seconds (uint64)
-    timeOutP1: string;    // Player 1 timeout timestamp (uint256)
-    timeOutP2: string;    // Player 2 timeout timestamp (uint256)
-    nLockTime: string;    // Lock time for the game (uint256)
-    isOdd: boolean;       // Whether the game is Odd/Even (bool)
-    player1: string;      // Player 1's address (address)
-    player2: string;      // Player 2's address (address)
-    optionP2: number;     // Player 2's option (int8)
-  };
-
-  function fetchGameData(rawGameData: any) {
-    
-    const gameData: GameData = {
-      hashOptionP1: rawGameData[0],
-      timeOut: rawGameData[1],
-      timeOutP1: rawGameData[2],
-      timeOutP2: rawGameData[3],
-      nLockTime: rawGameData[4],
-      isOdd: rawGameData[5],
-      player1: rawGameData[6],
-      player2: rawGameData[7],
-      optionP2: Number(rawGameData[8]),
-    };
-    return gameData;
-  }
-  
+  let keySeed = hexStringToUint8Array ("abcddbe576b4818846aa77e82f4ed5fa78f92766b141f282d36703886d196df39322abcddbe576b4818846aa77e82f4ed5fa78f92766b141f282d36703886d196df39322")
+  let gameKey = hre.ethers.keccak256(keySeed);
 
   const DEFAULT_BID = hre.ethers.parseEther("0.01");
 
@@ -78,6 +80,67 @@ describe("OddOrEven", function () {
     
     expect(gameData.optionP2).to.equal(-1);
   });
+
+
+  it("should init game", async function () {
+    const { oddOrEven, owner, player1, player2 } = await loadFixture(deployFixture);
+
+    const player1Instance = oddOrEven.connect(player1);
+
+    console.log("gameKey: ", gameKey);
+    let keygame: string = gameKey.substring(2, gameKey.length)
+
+    console.log("keygame: ", keygame);
+    console.log("keygame2: ", gameKey.substring(2));
+    let optionP1In: number = 3;
+
+    let optionP1str = optionP1In.toString(16);
+    console.log("optionP1str: ", optionP1str);
+
+    while(optionP1str.length % 2 === 1 )
+      optionP1str = "0" + optionP1str;
+
+    console.log("optionP1str: ", optionP1str);
+
+    let hashOptionP1In = (hre.ethers.keccak256(hexStringToUint8Array(keygame + optionP1str))).substring(2);
+
+    console.log("hashOptionP1In: ", hashOptionP1In);
+
+    let isOdd = false;
+
+    await player1Instance.playerInit(isOdd, hashOptionP1In, {value: DEFAULT_BID});
+
+    let gameData = fetchGameData(await oddOrEven.gameData());
+
+    console.log("Balance: ", hre.ethers.formatEther(await oddOrEven.getBalance()));
+    console.log("NLockTime: ", gameData.nLockTime);
+    console.log("timeOutP1: ", gameData.timeOutP1);
+  
+    expect(gameData.hashOptionP1).to.equal(hashOptionP1In);
+  });
+
+  it("should NOT init game (Invalid Bid)", async function () {
+    const { oddOrEven, owner, player1, player2 } = await loadFixture(deployFixture);
+
+    const player1Instance = oddOrEven.connect(player1);
+
+    let keygame: string = gameKey.substring(2, gameKey.length)
+    let optionP1In: number = 3;
+    let optionP1str = optionP1In.toString(16);
+
+    while(optionP1str.length % 2 === 1 )
+      optionP1str = "0" + optionP1str;
+
+    let hashOptionP1In = (hre.ethers.keccak256(hexStringToUint8Array(keygame + optionP1str))).substring(2);
+    let isOdd = false;
+
+    //await player1Instance.playerInit(isOdd, hashOptionP1In, {value: DEFAULT_BID - 1n});
+
+    await expect(player1Instance.playerInit(isOdd, hashOptionP1In, {value: DEFAULT_BID - 1n}))
+    .to.be.revertedWith("Invalid Bid");
+  });
+
+  
 
 
   /*
